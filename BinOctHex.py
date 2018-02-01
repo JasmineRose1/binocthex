2541e49916ab0f95d284e26d1b331b27c9551953
resilient-coders8 <resilient-coders8@resilieers8smbp.bc.edu>
2018-02-01 10:51:03 -0500

    first commit

---
+++ BinOctHex.py
@@ -0,0 +1,22 @@
+# description: This is my first program
+# author : ROSE-OLESCO, ARMELLE
+# date: January.17.2018
+
+# convert an entered integer into its binary equivalent
+#integerbin = bin(27)
+#print integerbin
+
+#convert an entered integer into its octal equivalent
+#integeroct = oct(27)
+#print integeroct
+
+#convert an entered integer into its hexadecimal equivalent
+#integerhex = hex(27)
+#print integerhex
+
+# a program that allows a user to input a number and then receive the converted binary, octal and hexadecimal equivalent
+num = input('Type in a number.')
+print bin(num)
+print oct(num)
+print hex(num)
+
